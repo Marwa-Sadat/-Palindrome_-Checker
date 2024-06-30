c9f1419375021e525a948d6cff586c8871a2eadc
unknown <marwasadat735@gmail.com>
2024-06-30 21:00:29 +0330

    solving lintres

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 document.getElementById('check-btn').onclick = function() {
-    const input = document.getElementById('text-input').value;
-    const resultDiv = document.getElementById('result');
+  const input = document.getElementById('text-input').value;
+  const resultDiv = document.getElementById('result');
   
-    if (input === '') {
-      alert('Please input a value');
-      return;
-    }
+  if (input === '') {
+  alert('Please input a value');
+  return;
+  }
   
-    const cleanedInput = cleanInput(input);
-    const isPalindrome = checkPalindrome(cleanedInput);
+  const cleanedInput = cleanInput(input);
+  const isPalindrome = checkPalindrome(cleanedInput);
   
-    if (isPalindrome) {
-      resultDiv.textContent = `${input} is a palindrome`;
-    } else {
-      resultDiv.textContent = `${input} is not a palindrome`;
-    }
+  if (isPalindrome) {
+  resultDiv.textContent = `${input} is a palindrome`;
+  } else {
+    resultDiv.textContent = `${input} is not a palindrome`;
+  }
   };
   
   function cleanInput(str) {
-    return str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  return str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
   }
   
   function checkPalindrome(str) {
-    return str === str.split('').reverse().join('');
+  return str === str.split('').reverse().join('');
   }  
